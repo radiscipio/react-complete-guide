394f0d6c3e460bca349101f1b9cd188086931c48
Radiscipio <robertdiscipio@gmail.com>
2020-06-09 16:21:50 -0400

    Reverted hook in app to class based component

--- src/App.js
+++ src/App.js
@@ -1,44 +1,40 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
-const app = props => {
-  const [personsState, setPersonsState] = useState({
-      persons: [
-        { name: 'Robert', age: 28 },
-        { name: 'Jordan', age: 27 },
-        { name: 'Nathan', age: 27 }
-      ],
-      otherState: 'some value'
-    });
+class App extends Component {
 
-  const switchNameHandler = () => {
+  state = {
+    persons: [
+      { name: 'Robert', age: 28 },
+      { name: 'Jordan', age: 27 },
+      { name: 'Nathan', age: 27 }
+    ],
+    otherState: 'some value'
+  }
+
+  switchNameHandler = () => {
     console.log('Was clicked');
-      setPersonsState({ 
-      persons: [
-        { name: 'Robert', age: 28 },
-        { name: 'Derek', age: 26 },
-        { name: 'Sela', age: 33 },
-      ]
-    })
-  };
+  }
 
+  render() {
     return (
       <div className="App">
         <h1>Hello there</h1>
-        <button onClick={switchNameHandler}>Switch Name</button>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
         <Person 
-          name={personsState.persons[0].name}
-          age={personsState.persons[0].age}
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
         />
         <Person 
-          name={personsState.persons[1].name}
-          age={personsState.persons[1].age}
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
         >and I like trains 🚂</Person>
         <Person 
-          name={personsState.persons[2].name}
-          age={personsState.persons[2].age} />
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age} />
       </div>
     );
   }
+};
 
-export default app;
+export default App;
