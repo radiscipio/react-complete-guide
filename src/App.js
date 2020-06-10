7c4581332f7c517e1f168ab9c0176ca9f199fca2
Radiscipio <robertdiscipio@gmail.com>
2020-06-10 16:43:17 -0400

    Person class shows if button pressed

--- src/App.js
+++ src/App.js
@@ -43,35 +43,40 @@ class App extends Component {
     const style = {
       backgroundColor: 'white',
       font: 'inherit',
-      border: '1px solid blue',
+      border: '1px solid #ccc',
       padding: '8px',
       cursor: 'pointer',
     };
 
+    let persons = null;
+
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+        <Person 
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+          click={this.switchNameHandler.bind(this, 'Robert Attilio di Scipio')}
+        />
+        <Person 
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+        >and I like trains 🚂
+        </Person>
+        <Person 
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+          changed={this.nameChangeHandler}
+        />
+      </div>
+      )
+    }
+
     return (
       <div className="App">
         <h1>Hello there</h1>
-        <button stlyle={style} onClick={this.togglePersonsHandler}>Switch Name</button>
-        { 
-          this.state.showPersons === true ?
-          <div>
-            <Person 
-              name={this.state.persons[0].name}
-              age={this.state.persons[0].age}
-              click={this.switchNameHandler.bind(this, 'Robert Attilio di Scipio')}
-            />
-            <Person 
-              name={this.state.persons[1].name}
-              age={this.state.persons[1].age}
-            >and I like trains 🚂
-            </Person>
-            <Person 
-              name={this.state.persons[2].name}
-              age={this.state.persons[2].age}
-              changed={this.nameChangeHandler}
-            />
-          </div> : null
-        }
+        <button style={style} onClick={this.togglePersonsHandler}>Toggle Persons</button>
+        {persons}
       </div>
     );
   };
