3eff172d1eda4c9794e0ced48359cb2bbeeaa8b5
Radiscipio <robertdiscipio@gmail.com>
2020-10-13 18:09:13 -0400

    Text color change on person deletion

--- src/App.js
+++ src/App.js
@@ -70,9 +70,19 @@ class App extends Component {
       style.backgroundColor = 'red';
     };
 
+    const classes = []
+
+    if (this.state.persons.length <= 2) {
+      classes.push('red')
+    }
+    if (this.state.persons.length <= 1) {
+      classes.push('bold')
+    }
+
     return (
       <div className="App">
         <h1>Hello there</h1>
+          <p className={classes.join(' ')}>This is working!</p>
         <button style={style} 
         onClick={this.togglePersonsHandler}>Toggle Persons</button>
         {persons}
